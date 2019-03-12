5a624a7d05ddfc3067fb1c27bf1eb18bc4b2fc12
JeffHsu <toy7755679@gmail.com>
2019-03-12 22:58:24 +0800

    finish Luhn Algorithm

--- credit_card.rb
+++ credit_card.rb
@@ -2,19 +2,25 @@ require_relative './luhn_validator.rb'
 require 'json'
 
 class CreditCard
-  # TODO: mixin the LuhnValidator using an 'include' statement
+  include LuhnValidator
 
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
-    # TODO: initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
   def to_json
     {
-      # TODO: setup the hash with all instance vairables to serialize into json
+      "number" => @number,
+      "expiration_date" => @expiration_date,
+      "owner" => @owner,
+      "credit_network" => @credit_network
     }.to_json
   end
 
@@ -25,21 +31,18 @@ class CreditCard
 
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
-    # TODO: deserializing a CreditCard object
+    creditCard_json_object = JSON.parse(card_s)
+    CreditCard.new(creditCard_json_object["number"], creditCard_json_object["expiration_date"], creditCard_json_object["owner"], creditCard_json_object["credit_network"])
   end
 
   # return a hash of the serialized credit card object
   def hash
-    # TODO: implement this method
-    #   - Produce a hash (using default hash method) of the credit card's
-    #     serialized contents.
-    #   - Credit cards with identical information should produce the same hash
+    to_s.hash
   end
 
   # return a cryptographically secure hash
   def hash_secure
-    # TODO: implement this method
-    #   - Use sha256 from openssl to create a cryptographically secure hash.
-    #   - Credit cards with identical information should produce the same hash
+    sha256 = OpenSSL::Digest::SHA256.new
+    digest = sha256.digest(to_s)
   end
 end
--- luhn_validator.rb
+++ luhn_validator.rb
@@ -5,7 +5,16 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
+    # reverse the array and double the value of every second digit
+    nums_a.reverse!.each_index { |i| nums_a[i] = nums_a[i]*2 if i%2 == 1 }
+    sum_digits = 0
+    # If the result of this doubling operation is greater than 9, then add the digits of the result
+    nums_a.each_index do |i|
+      nums_a[i] = ((nums_a[i]/10)%10) + (nums_a[i]%10) if nums_a[i] >= 10
+      sum_digits += nums_a[i] unless i == 0 
+    end
+    return true if nums_a[0] == (10 - (sum_digits%10)) ||  (sum_digits%10 == 0 && nums_a[0] == 0)
 
-    # TODO: use the integers in nums_a to validate its last check digit
+    false
   end
 end
