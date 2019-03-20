26f10eeb3e60b9ee11581bbbf56efd341d1c7d07
KevinYang515 <kevin850515123@gmail.com>
2019-03-20 23:02:47 +0800

    Finish substitution_cipher

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -7,6 +7,9 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using caesar cipher
+      x = document.to_s.each_char.map{|a| a.ord + key }
+
+      x.map{|a| a.chr}.join("")
     end
 
     # Decrypts String document using integer key
@@ -16,6 +19,9 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
+      x = document.to_s.each_char.map{|a| a.ord - key}
+
+      x.map{|a| a.chr}.join("")
     end
   end
 
@@ -27,15 +33,25 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      def self.hashfun(number, key)
+        number.to_i * 2 + key
+      end
+      
+      document.to_s.each_char.map{|a| hashfun(a.ord, key).chr}.join("")
     end
-
-    # Decrypts String document using integer key
-    # Arguments:
-    #   document: String
-    #   key: Fixnum (integer)
-    # Returns: String
+    
+        # Decrypts String document using integer key
+        # Arguments:
+        #   document: String
+        #   key: Fixnum (integer)
+        # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      def self.hashfun(number, key)
+        (number - key) / 2
+      end
+    
+      document.to_s.each_char.map{|a| hashfun(a.ord, key).chr}.join("")
     end
   end
 end
