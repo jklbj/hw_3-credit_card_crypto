0daf0f8316ad50022486c19f86f9038d4ec4fd75
JeffHsu <toy7755679@gmail.com>
2019-03-20 14:34:56 +0800

    finish crypto_spec

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -2,6 +2,7 @@
 
 require_relative '../credit_card'
 require_relative '../substitution_cipher'
+require_relative '../double_trans_cipher'
 require 'minitest/autorun'
 
 describe 'Test card info encryption' do
@@ -9,6 +10,7 @@ describe 'Test card info encryption' do
     @cc = CreditCard.new('4916603231464963', 'Mar-30-2020',
                          'Soumya Ray', 'Visa')
     @key = 3
+    @transpositionKey = [2,1,3,4]
   end
 
   describe 'Using Caesar cipher' do
@@ -39,6 +41,17 @@ describe 'Test card info encryption' do
     end
   end
 
-  # TODO: Add tests for double transposition and modern symmetric key ciphers
-  #       Can you DRY out the tests using metaprogramming? (see lecture slide)
+  describe 'Using double transposition cipher' do
+    it 'should encrypt card information' do
+      enc = DoubleTranspositionCipher.encrypt(@cc, @transpositionKey)
+      enc.wont_equal @cc.to_s
+      enc.wont_be_nil
+    end
+
+    it 'should decrypt text' do
+      enc = DoubleTranspositionCipher.encrypt(@cc, @transpositionKey)
+      dec = DoubleTranspositionCipher.decrypt(enc, @transpositionKey)
+      dec.must_equal @cc.to_s
+    end
+  end
 end
