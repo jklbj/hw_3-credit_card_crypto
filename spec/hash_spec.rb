f15e066016f63193beccd07229a137ccaedf0c3a
JeffHsu <toy7755679@gmail.com>
2019-03-26 22:51:30 +0800

    finish hash spec

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -1,7 +1,6 @@
 require_relative '../credit_card'
 require 'minitest/autorun'
 
-# Feel free to replace the contents of cards with data from your own yaml file
 card_details = [
   { num: '4916603231464963',
     exp: 'Mar-30-2020',
@@ -24,25 +23,126 @@ end
 describe 'Test hashing requirements' do
   describe 'Test regular hashing' do
     describe 'Check hashes are consistently produced' do
-      # TODO: Check that each card produces the same hash if hashed repeatedly
+      it 'Check the first card has the same hash when it is consistently produced' do
+        card = cards[0]
+        hashed_card_1 = card.hash
+        hashed_card_2 = card.hash
+        hashed_card_1.must_equal hashed_card_2
+      end
+
+      it 'Check the second card has the same hash when it is consistently produced' do
+        card = cards[1]
+        hashed_card_1 = card.hash
+        hashed_card_2 = card.hash
+        hashed_card_1.must_equal hashed_card_2
+      end
+
+      it 'Check the third card has the same hash when it is consistently produced' do
+        card = cards[2]
+        hashed_card_1 = card.hash
+        hashed_card_2 = card.hash
+        hashed_card_1.must_equal hashed_card_2
+      end
     end
 
     describe 'Check for unique hashes' do
-      # TODO: Check that each card produces a different hash than other cards
+      it 'Check the first card and the second card do not have the same hash' do
+        card_1 = cards[0]
+        card_2 = cards[1]
+        hashed_card_1 = card_1.hash
+        hashed_card_2 = card_2.hash
+        hashed_card_1.wont_equal hashed_card_2
+      end
+
+      it 'Check the first card and the third card do not have the same hash' do
+        card_1 = cards[0]
+        card_3 = cards[2]
+        hashed_card_1 = card_1.hash
+        hashed_card_3 = card_3.hash
+        hashed_card_1.wont_equal hashed_card_3
+      end
+
+      it 'Check the second card and the third card do not have the same hash' do
+        card_2 = cards[1]
+        card_3 = cards[2]
+        hashed_card_2 = card_2.hash
+        hashed_card_3 = card_3.hash
+        hashed_card_2.wont_equal hashed_card_3
+      end
     end
-  end
+  end  
 
   describe 'Test cryptographic hashing' do
     describe 'Check hashes are consistently produced' do
-      # TODO: Check that each card produces the same hash if hashed repeatedly
+      it 'Check the first card has the same hash when it is consistently produced' do
+        card = cards[0]
+        hashed_card_1 = card.hash_secure
+        hashed_card_2 = card.hash_secure
+        hashed_card_1.must_equal hashed_card_2
+      end
+
+      it 'Check the second card has the same hash when it is consistently produced' do
+        card = cards[1]
+        hashed_card_1 = card.hash_secure
+        hashed_card_2 = card.hash_secure
+        hashed_card_1.must_equal hashed_card_2
+      end
+
+      it 'Check the third card has the same hash when it is consistently produced' do
+        card = cards[2]
+        hashed_card_1 = card.hash_secure
+        hashed_card_2 = card.hash_secure
+        hashed_card_1.must_equal hashed_card_2
+      end
     end
 
     describe 'Check for unique hashes' do
-      # TODO: Check that each card produces a different hash than other cards
+      it 'Check the first card and the second card do not have the same hash' do
+        card_1 = cards[0]
+        card_2 = cards[1]
+        hashed_card_1 = card_1.hash_secure
+        hashed_card_2 = card_2.hash_secure
+        hashed_card_1.wont_equal hashed_card_2
+      end
+
+      it 'Check the first card and the third card do not have the same hash' do
+        card_1 = cards[0]
+        card_3 = cards[2]
+        hashed_card_1 = card_1.hash_secure
+        hashed_card_3 = card_3.hash_secure
+        hashed_card_1.wont_equal hashed_card_3
+      end
+
+      it 'Check the second card and the third card do not have the same hash' do
+        card_2 = cards[1]
+        card_3 = cards[2]
+        hashed_card_2 = card_2.hash_secure
+        hashed_card_3 = card_3.hash_secure
+        hashed_card_2.wont_equal hashed_card_3
+      end
     end
 
     describe 'Check regular hash not same as cryptographic hash' do
-      # TODO: Check that each card's hash is different from its hash_secure
+      it 'Check the first card does not have the same hash with hash_secure' do
+        card = cards[0]
+        hash_card = card.hash
+        hash_secure_card = card.hash_secure
+        hash_card.wont_equal hash_secure_card
+      end
+
+      it 'Check the second card does not have the same hash with hash_secure' do
+        card = cards[1]
+        hash_card = card.hash
+        hash_secure_card = card.hash_secure
+        hash_card.wont_equal hash_secure_card
+      end
+
+      it 'Check the third card does not have the same hash with hash_secure' do
+        card = cards[2]
+        hash_card = card.hash
+        hash_secure_card = card.hash_secure
+        hash_card.wont_equal hash_secure_card
+      end
     end
   end
 end
