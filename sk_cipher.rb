3b85d38397cf500b1717baf0464743c14d4f7966
KevinYang515 <kevin850515123@gmail.com>
2019-03-27 12:27:54 +0800

    Finish Modern Symmetric Cipher

--- sk_cipher.rb
+++ sk_cipher.rb
@@ -3,8 +3,6 @@ require 'base64'
 
 module ModernSymmetricCipher
 
-   
-
   def self.generate_new_key
     # TODO: Return a new key as a Base64 string
     secret_box = RbNaCl::Random.random_bytes(RbNaCl::SecretBox.key_bytes)
